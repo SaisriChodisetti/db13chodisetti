d235463981a8ea43dd0defc055ca87e132b683cb
SaisriChodisetti <chodisettisaisri@gmail.com>
2021-11-18 22:52:21 -0600

    Sc-1

--- routes/plant.js
+++ routes/plant.js
@@ -3,6 +3,7 @@ const plant_controller = require('../controllers/plant');
 var router = express.Router();
 
 // router.get('/delete', plant_controller.plant_delete_Page);
-router.get('/detail', plant_controller.plant_view_one_Page);
+router.get('/', plant_controller.plant_view_all_Page);
+router.get('/update', plant_controller.plant_update_Page);
 
 module.exports = router;
