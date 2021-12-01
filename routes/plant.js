2c00d694f63c968c26ed921145d0bf078848d8e0
SaisriChodisetti <chodisettisaisri@gmail.com>
2021-12-01 16:28:53 -0600

    Part 4

--- routes/plant.js
+++ routes/plant.js
@@ -4,6 +4,14 @@ var router = express.Router();
 
 // router.get('/delete', plant_controller.plant_delete_Page);
 router.get('/', plant_controller.plant_view_all_Page);
-router.get('/update', plant_controller.plant_update_Page);
+
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+router.get('/update', secured, plant_controller.plant_update_Page);
 
 module.exports = router;
